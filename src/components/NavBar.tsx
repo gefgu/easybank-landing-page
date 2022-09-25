36b54cadb2a803afe0e128ffaa82a3f0ecca70a1
Gustavo Santos <gefgu@hotmail.com>
2022-09-25 11:03:18 -0300

    feat: add styling to button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 
 const NavBar: React.FunctionComponent = () => {
   return (
-    <header className="max-w-7xl mx-auto flex justify-between items-center h-24 px-12">
+    <header className="max-w-7xl mx-auto flex justify-between items-center h-24 px-12 py-2">
       <a href="#"><img src="/logo.svg" alt="logo" /></a>
       <nav className="flex gap-x-8 grayish-blue font-light">
         <a href="#">Home</a>
@@ -11,7 +11,7 @@ const NavBar: React.FunctionComponent = () => {
         <a href="#">Blog</a>
         <a href="#">Carrers</a>
       </nav>
-      <button>Request Invite</button>
+      <button className="px-10 py-3 rounded-full text-white gradient font-bold">Request Invite</button>
     </header>
   );
 };
