b6d2633984be04f64f7f31cc3dfbe0494e58b822
Gustavo Santos <gefgu@hotmail.com>
2022-09-25 16:29:44 -0300

    feat: add hover state for links in nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,14 @@ import type React from "react";
 
 const NavBar: React.FunctionComponent = () => {
   return (
-    <header className="max-w-7xl mx-auto flex justify-between items-center h-24 px-12 py-2">
+    <header className="max-w-7xl mx-auto flex justify-between items-center h-24 px-12 pt-4">
       <a href="#"><img src="/logo.svg" alt="logo" /></a>
-      <nav className="flex gap-x-8 grayish-blue font-light">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
-        <a href="#">Blog</a>
-        <a href="#">Carrers</a>
+      <nav className="flex gap-x-8 grayish-blue font-light h-full flex items-center">
+        <a href="#" className="h-full flex items-center bottom-border-hover">Home</a>
+        <a href="#" className="h-full flex items-center bottom-border-hover">About</a>
+        <a href="#" className="h-full flex items-center bottom-border-hover">Contact</a>
+        <a href="#" className="h-full flex items-center bottom-border-hover">Blog</a>
+        <a href="#" className="h-full flex items-center bottom-border-hover">Carrers</a>
       </nav>
       <button className="px-10 py-3 rounded-full text-white gradient font-bold">Request Invite</button>
     </header>
