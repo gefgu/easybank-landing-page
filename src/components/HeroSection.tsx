20b7858002a1ddd2e62f8dd0eec0cca94741cb2c
Gustavo Santos <gefgu@hotmail.com>
2022-09-26 15:08:36 -0300

    feat: hero section fixes for design

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,9 +4,9 @@ import InviteButton from "./InviteButton";
 const HeroSection: React.FunctionComponent = () => {
   return (
     <main className="bg-light-grey grid grid-cols-2">
-      <section className="flex flex-col gap-8 items-start justify-center px-12 py-32 2xl:pl-72">
-        <h1 className="text-5xl">Next generation digital banking</h1>
-        <p>
+      <section className="flex flex-col gap-12 items-start justify-center px-12 py-28 2xl:pl-72">
+        <h1 className="text-5xl dark-blue font-light">Next generation digital banking</h1>
+        <p className="grayish-blue">
           Take your financial life online. Your Easybank account will be
           one-stop-shop for spending, saving, budgeting, investing, and much
           more.
