52ef71ab8b9a5763289ea6f2d2fcb9c1df28d137
Gustavo Santos <gefgu@hotmail.com>
2022-09-26 16:40:50 -0300

    feat: add scroll animations to hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,24 +1,36 @@
 import type React from "react";
 import InviteButton from "./InviteButton";
+import { motion } from "framer-motion";
 
 const HeroSection: React.FunctionComponent = () => {
   return (
     <main className="bg-light-grey grid grid-cols-1 sm:grid-cols-2 gap-12">
-      <section className="flex flex-col gap-12 items-start justify-center px-12 py-28 2xl:pl-72">
-        <h1 className="text-5xl dark-blue font-light text-center sm:text-start">Next generation digital banking</h1>
+      <motion.section
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        className="flex flex-col gap-12 items-start justify-center px-12 py-28 2xl:pl-72"
+      >
+        <h1 className="text-5xl dark-blue font-light text-center sm:text-start">
+          Next generation digital banking
+        </h1>
         <p className="grayish-blue leading-relaxed text-justify">
           Take your financial life online. Your Easybank account will be
           one-stop-shop for spending, saving, budgeting, investing, and much
           more.
         </p>
         <InviteButton />
-      </section>
+      </motion.section>
       <div className="relative order-first sm:order-last">
         <div className="overflow-hidden">
           <img src="/bg-intro-desktop.svg" className="bg-intro" />
         </div>
         <div className="overflow-x-hidden">
-          <img src="/image-mockups.png" className="mockups" />
+          <motion.img
+            src="/image-mockups.png"
+            className="mockups"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+          />
         </div>
       </div>
     </main>
