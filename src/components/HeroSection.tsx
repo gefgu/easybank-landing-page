1cd5154485acee52def93319afcb1d9124f26de5
Gustavo Santos <gefgu@hotmail.com>
2022-09-26 16:22:47 -0300

    feat: make hero section responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import InviteButton from "./InviteButton";
 
 const HeroSection: React.FunctionComponent = () => {
   return (
-    <main className="bg-light-grey grid grid-cols-2">
+    <main className="bg-light-grey grid grid-cols-1 sm:grid-cols-2 gap-12">
       <section className="flex flex-col gap-12 items-start justify-center px-12 py-28 2xl:pl-72">
         <h1 className="text-5xl dark-blue font-light">Next generation digital banking</h1>
         <p className="grayish-blue leading-relaxed">
@@ -13,7 +13,7 @@ const HeroSection: React.FunctionComponent = () => {
         </p>
         <InviteButton />
       </section>
-      <div className="relative">
+      <div className="relative order-first sm:order-last">
         <div className="overflow-hidden">
           <img src="/bg-intro-desktop.svg" className="bg-intro" />
         </div>
