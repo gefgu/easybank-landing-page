cf1d5465a7cb73f46d7c46a0bfcae86263c5f381
Gustavo Santos <gefgu@hotmail.com>
2022-09-26 16:42:41 -0300

    feat: add scrool animations to why

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,4 +1,5 @@
 import type React from "react";
+import { motion } from "framer-motion";
 
 interface FeatureProps {
   image: string;
@@ -12,13 +13,15 @@ const Feature: React.FunctionComponent<FeatureProps> = ({
   title,
 }: FeatureProps) => {
   return (
-    <section className="flex flex-col gap-6 items-center md:items-start">
+    <motion.section
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      className="flex flex-col gap-6 items-center md:items-start"
+    >
       <img src={image} alt="placeholder" />
       <h3 className="text-xl dark-blue font-light">{title}</h3>
-      <p className="text-sm grayish-blue leading-relaxed">
-        {text}
-      </p>
-    </section>
+      <p className="text-sm grayish-blue leading-relaxed">{text}</p>
+    </motion.section>
   );
 };
 
