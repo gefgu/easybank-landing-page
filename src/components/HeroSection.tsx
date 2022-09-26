8edfaab99a27c1a74d2bbfdaa959210aee2d191e
Gustavo Santos <gefgu@hotmail.com>
2022-09-26 16:29:39 -0300

    feat: improve text style in hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,8 +5,8 @@ const HeroSection: React.FunctionComponent = () => {
   return (
     <main className="bg-light-grey grid grid-cols-1 sm:grid-cols-2 gap-12">
       <section className="flex flex-col gap-12 items-start justify-center px-12 py-28 2xl:pl-72">
-        <h1 className="text-5xl dark-blue font-light">Next generation digital banking</h1>
-        <p className="grayish-blue leading-relaxed">
+        <h1 className="text-5xl dark-blue font-light text-center sm:text-start">Next generation digital banking</h1>
+        <p className="grayish-blue leading-relaxed text-justify">
           Take your financial life online. Your Easybank account will be
           one-stop-shop for spending, saving, budgeting, investing, and much
           more.
